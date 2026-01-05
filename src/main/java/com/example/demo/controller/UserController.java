951da1ba023f7535396b53ab79fc151aaacf5461
gialongchuai <gialongchuai@gmail.com>
2026-01-05 16:36:55 +0700

    data response

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,38 +1,47 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.request.UserRequestDTO;
+import com.example.demo.dto.response.ResponseData;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @PostMapping("/")
-    public String addUser(@Valid @RequestBody UserRequestDTO requestDTO) {
+    public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO requestDTO) {
         System.out.println(requestDTO);
-        return "User added!";
+        return new ResponseData<>(HttpStatus.CREATED.value(), "User added successfully!", 1);
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO requestDTO) {
+    public ResponseData<?> updateUser(@PathVariable int userId, @RequestBody UserRequestDTO requestDTO) {
         System.out.println("Dang update userID: " + userId);
-        return "User updated!";
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated!");
     }
 
     @PatchMapping("/{userId}")
-    public String changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
-        return "User status changed!";
+    public ResponseData<?> changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User status changed!");
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId) {
-        return "User deleted!";
+    public ResponseData<?> deleteUser(@PathVariable int userId) {
+        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted!");
     }
 
     @GetMapping("/{userId}")
-    public UserRequestDTO getUser(@PathVariable int userId) {
-        return new UserRequestDTO("Tay", "Java", "abc@gmail.com","0321123123");
+    public ResponseData<UserRequestDTO> getUser(@PathVariable int userId) {
+        return new ResponseData<>(HttpStatus.OK.value(), "user", new UserRequestDTO("Tay", "Java", "abc@gmail.com", "0321123123"));
+    }
+
+    @GetMapping("/list")
+    public ResponseData<List<UserRequestDTO>> getUserList() {
+        return new ResponseData<>(HttpStatus.OK.value(), "list user", List.of(new UserRequestDTO("Tay", "Java", "abc@gmail.com", "0321123123"),
+                new UserRequestDTO("Tay", "Java", "abc@gmail.com", "0321123123")));
     }
 }
