bf03c3fe6eb324ecd269b84cb11df3952a6ea747
gialongchuai <gialongchuai@gmail.com>
2026-01-07 08:47:37 +0700

    config cors

---
+++ src/main/java/com/example/demo/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:63342")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
