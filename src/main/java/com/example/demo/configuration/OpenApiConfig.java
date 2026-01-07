8ff3ce40214aeb196395b87eb0b59c8a94ab4846
gialongchuai <gialongchuai@gmail.com>
2026-01-07 11:19:57 +0700

    api_document_swagger_postman

---
+++ src/main/java/com/example/demo/configuration/OpenApiConfig.java
@@ -0,0 +1,59 @@
+package com.example.demo.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+// curl -X 'GET' \
+//  'http://localhost:8080/user/1' \
+//  -H 'accept: */*' \
+//  -H 'Authorization: Bearer xxx'
+// */
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${open.api.title}") String title
+            , @Value("${open.api.version}") String version
+            , @Value("${open.api.description}") String description
+            , @Value("${open.api.serverUrl}") String serverUrl
+            , @Value("${open.api.serverName}") String serverName) {
+        return new OpenAPI().info(new Info()
+                .title(title)
+                .version(version)
+                .description(description)
+                .license(new License()
+                        .name("API license")
+                        .url("http://domain.vn/license")))
+                .servers(List.of(new Server()
+                        .url(serverUrl)
+                        .description(serverName)));
+//                .components(new Components() // yêu cần phải có token đi kèm request
+//                        .addSecuritySchemes(
+//                                "bearerAuth",
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")))
+//                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("api-service-1") // nó chia thành các group trên đầu để selection
+                .packagesToScan("com.example.demo.controller")
+                .build();
+    }
+}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.demo.configuration.Translator;
 import com.example.demo.dto.request.UserRequestDTO;
 import com.example.demo.dto.response.ResponseData;
 import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -15,38 +17,45 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User Controller")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Add user", description = "API create a user!")
     @PostMapping("/")
     public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO requestDTO) {
 //        System.out.println(requestDTO);
         return new ResponseData<>(HttpStatus.CREATED.value(), Translator.toLocale("user.add.success"), userService.addUser(requestDTO));
     }
 
+    @Operation(summary = "Update user", description = "API update a user!")
     @PutMapping("/{userId}")
     public ResponseData<?> updateUser(@PathVariable int userId, @RequestBody UserRequestDTO requestDTO) {
         System.out.println("Dang update userID: " + userId);
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.update.success"));
     }
 
+    @Operation(summary = "Update status user", description = "API update status a user!")
     @PatchMapping("/{userId}")
     public ResponseData<?> changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.update.success"));
     }
 
+    @Operation(summary = "Delete a user", description = "API delete a user!")
     @DeleteMapping("/{userId}")
     public ResponseData<?> deleteUser(@PathVariable int userId) {
         return new ResponseData<>(HttpStatus.NO_CONTENT.value(), Translator.toLocale("user.delete.success"));
     }
 
+    @Operation(summary = "Get information a user", description = "API get a user!")
     @GetMapping("/{userId}")
     public ResponseData<UserRequestDTO> getUser(@PathVariable @Min(1) @Max(10) @Valid int userId) { // new UserRequestDTO("Tay", "Java", "abc@gmail.com", "0321123123")
         return new ResponseData<>(HttpStatus.OK.value(), Translator.toLocale("user.get.success"), userService.getUser(userId));
     }
 
+    @Operation(summary = "Get information a user list", description = "API get list user!")
     @GetMapping("/list")
     public ResponseData<List<UserRequestDTO>> getUserList() {
         return new ResponseData<>(HttpStatus.OK.value(), Translator.toLocale("user.get.success"), List.of(new UserRequestDTO("Tay", "Java", "abc@gmail.com", "0321123123"),
